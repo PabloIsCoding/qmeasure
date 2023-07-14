f7475c0025a937ec5600b933b20223a9ccd25501
PabloIsCoding <pabloparsil@gmail.com>
2023-07-14 21:14:43 +0200

    Fix bug in the number of timesteps, saner code

--- binomial.py
+++ binomial.py
@@ -23,7 +23,7 @@ def _build_log_matrix(value1, value2, N):
         This will typically be d, the raw return in the case the underlying goes
         down.
     N : int
-        size of the final array NxN.
+        Number of timesteps. The resulting matrix is (N+1)x(N+1)
 
     Returns
     -------
@@ -31,20 +31,20 @@ def _build_log_matrix(value1, value2, N):
         DESCRIPTION.
 
     """
-    result = np.ones((N, N))
-    V1 = np.zeros(N)
+    result = np.ones((N+1, N+1))
+    V1 = np.zeros(N+1)
     V1[1:] = value1
     result[0, :] = V1.cumsum()
-    for i in range(1, N):
+    for i in range(1, N+1):
         result[i] = result[0] - value1*i + value2*i
     return result
 
 def _build_matrix(value1, value2, N):
-    result = np.ones((N, N))
-    V1 = np.ones(N)
+    result = np.ones((N+1, N+1))
+    V1 = np.ones(N+1)
     V1[1:] = value1
     result[0, :] = V1.cumprod()
-    for i in range(1, N):
+    for i in range(1, N+1):
         result[i] = result[0] / value1**i * value2**i
     return np.triu(result)
 
@@ -62,11 +62,11 @@ def _build_underlying_matrix(S_0, u, N):
     u : float
         Raw return in the case .
     N : int
-        Size of the matrix (the number of steps therefore is N-1).
+        Number of timesteps. The resulting matrix is (N+1)x(N+1)
 
     Returns
     -------
-    NxN float-64 numpy array
+    (N+1)x(N+1) float-64 numpy array
         The array constains all the possible values the underlying can take.
     """
     
@@ -76,9 +76,9 @@ def _build_underlying_matrix(S_0, u, N):
 
 def _build_probabilities(p, N, timestep_number):
     q = 1-p
-    P = np.zeros(N)
     rv = binom(N, p)
-    return rv.pmf(np.arange(N-1, -1, -1))
+    x = np.arange(N, -1, -1)
+    return rv.pmf(x)
 
 def _european_payoffs(underlying_matrix, K, kind='C'):
     if kind=='C':
@@ -87,9 +87,11 @@ def _european_payoffs(underlying_matrix, K, kind='C'):
         payoffs = np.clip(K-underlying_matrix[:, -1], a_min=0, a_max=np.inf)
     return payoffs
 
-def value_european_option_binomial(S_0, T, sigma, K, r, kind='C', N=1000):
+def value_european_option_binomial(S_0, T, sigma, K, r, q=0, kind='C', N=1000):
     """
-    
+    Values an european option with the binomial method. The underlying could be
+    a stock, a exchange rate or a future. If it is a future, both r and q should
+    be zero.
 
     Parameters
     ----------
@@ -104,6 +106,9 @@ def value_european_option_binomial(S_0, T, sigma, K, r, kind='C', N=1000):
         Strike price of the option.
     r : float
         Constant interest rate over the period of the option.
+    q : float
+        Constant continuous dividend yield, or constant foreign risk free rate 
+        in the case of FX options.
     kind : String, optional
         'C' for call option, anything else for put option. The default is 'C'.
     N : int, optional
@@ -120,7 +125,8 @@ def value_european_option_binomial(S_0, T, sigma, K, r, kind='C', N=1000):
     dt = T/float(N)
     u = np.exp(sigma*np.sqrt(dt))
     d = 1/u
-    p = (np.exp(r*dt) - d)/(u - d)
+    a = np.exp((r-q)*dt)
+    p = (a - d)/(u - d)
     S = _build_underlying_matrix(S_0, u, N)
     P = _build_probabilities(p, N, N)
     payoffs = _european_payoffs(S, K, kind)
@@ -128,11 +134,11 @@ def value_european_option_binomial(S_0, T, sigma, K, r, kind='C', N=1000):
 
 def calculate_u(sigma, dt, r):
     """
-    Returns the u parameter for the binomial model. The typical formula
-    you read in books is an approximation. One of these two returned values
-    is the actual u. In practice this doesn't matter much and that's
-    why I didn't bother to use it in the end, but I wanted to keep it just in
-    case.
+    Returns the u parameter for the binomial model for a non-dividend paying stock. 
+    The typical formula you read in books is an approximation. One of the two 
+    returned values is the actual u. In practice this doesn't matter much and 
+    that's why I didn't bother to use it in the end, but I wanted to keep it 
+    just in case.
 
     Parameters
     ----------
@@ -155,7 +161,7 @@ def calculate_u(sigma, dt, r):
     u2 = 1/2*e**(-r*dt)*(np.sqrt((sigma**2*(-dt) - e**(2*r*dt) - 1)**2 - 4*e**(2*r*dt)) + sigma**2*dt + e**(2*r*dt) + 1)
     return u1, u2
 
-def value_european_option_BS(S_0, T, sigma, K, r, kind='C'):
+def value_european_option_BS(S_0, T, sigma, K, r, q=0, kind='C'):
     """
     Value of a european option according to the Black-Scholes model.
 
@@ -172,6 +178,8 @@ def value_european_option_BS(S_0, T, sigma, K, r, kind='C'):
         Strike price of the option.
     r : float
         Constant interest rate over the period of the option.
+    q : float
+        Constant continuous dividend yield.
     kind : String, optional
         'C' for call option, anything else for put option. The default is 'C'.
 
@@ -181,29 +189,30 @@ def value_european_option_BS(S_0, T, sigma, K, r, kind='C'):
         Value of the option in monetary units.
 
     """
-    d1 = (np.log(S_0/K) + (r+sigma**2/2)*T)/(sigma*np.sqrt(T))
+    d1 = (np.log(S_0/K) + (r-q+sigma**2/2)*T)/(sigma*np.sqrt(T))
     d2 = d1 - sigma*np.sqrt(T)
     N = norm.cdf
     if kind=='C':
-        value = S_0*N(d1) - K*np.exp(-r*T)*N(d2)
+        value = S_0*np.exp(-q*T)*N(d1) - K*np.exp(-r*T)*N(d2)
     else:
-        value = K*np.exp(-r*T)*N(-d2) - S_0*N(-d1)
+        value = K*np.exp(-r*T)*N(-d2) - S_0*np.exp(-q*T)*N(-d1)
     return value
 
 if __name__=='__main__':
     import matplotlib.pyplot as plt
-    Ns = list(range(3, 30, 2))+[40, 50, 60, 70, 100, 120, 150, 300, 600, 900, 1200, 1600, 2200, 3000, 6000, 9000]
+    Ns = np.arange(2, 501)
     S_0 = 50
-    T = 1
-    r = 0.06
-    sigma = 0.4
-    K = 50
-    values = [value_european_option_binomial(S_0=S_0, T=T, sigma=sigma, K=K, r=r, kind='C', N=n) for n in Ns]
+    T = 2.
+    r = 0.05
+    q = 0.
+    sigma = 0.3
+    K = 52
+    values = [value_european_option_binomial(S_0=S_0, T=T, sigma=sigma, K=K, r=r, q=q, kind='P', N=n) for n in Ns]
     print(values)
-    bs_value = value_european_option_BS(S_0=S_0, T=T, sigma=sigma, K=K, r=r, kind='C')
+    bs_value = value_european_option_BS(S_0=S_0, T=T, sigma=sigma, K=K, r=r, q=q, kind='P')
     fig, ax = plt.subplots()
     ax.plot(Ns, np.repeat(bs_value, len(Ns)), label='Black Scholes')
-    ax.plot(Ns, values, label='Binomial')
+    ax.plot(Ns, values, label='Binomial', lw=0.5)
     ax.set_xlabel('Number of timesteps')
     ax.set_ylabel('Value')
     ax.legend()
